bd71d26c917984a596b65422db74d47e8cdefc8a
Zach Jepsen <jepsenzd@mail.uc.edu>
2019-12-09 19:56:18 -0500

    Added more UI for login and signup pages

---
+++ possibility/possibility/Model/restaurant.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace possibility.Models
+{
+        public readonly struct Restaurant
+        {
+            public string Name { get; }
+
+            public string City { get; }
+
+            public double Latitude { get; }
+
+            public double Longitude { get; }
+
+            public Restaurant(string name, string city, double latitude, double longitude)
+            {
+                Name = name;
+                City = city;
+                Latitude = latitude;
+                Longitude = longitude;
+            }
+        }
+}
